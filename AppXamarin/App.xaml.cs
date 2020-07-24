d8e144fdd5a867828eac343712b5811104be2a75
Maxim Andreevich <maxim090491@outlook.com>
2020-07-24 14:57:08 +0300

    Revert "Check the connection GitHub"
    This reverts commit 40ff059e78aa5494555542ad93a57fadaa8693d3.

--- AppXamarin/App.xaml.cs
+++ AppXamarin/App.xaml.cs
@@ -21,13 +21,7 @@ namespace AppXamarin
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
     sealed partial class App : Application
-
-        
     {
-        void Oleg()
-        {
-            //For sent Git
-        }
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
